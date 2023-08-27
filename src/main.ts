50840e40b6a587dd2a364a31e763a7f68971e6f8
amphipath <amphipath@users.noreply.github.com>
2023-08-27 19:50:52 +0800

    start to add skill data

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
-import './style.css'
+
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 
 
---
+++ src/skillData.ts
@@ -0,0 +1,41 @@
+type SkillData = {
+  id: number;
+  name: string;
+  mseaVideoUrl: string;
+  kmsVideoUrl: string;
+};
+
+const skillData: SkillData[] = [
+  {
+    id: 22111012,
+    name: "Dragon Swift",
+    mseaVideoUrl: "https://i.imgur.com/9p8HSgK.mp4",
+    kmsVideoUrl: "",
+  },
+  {
+    id: 22110014,
+    name: "Swift of Wind",
+    mseaVideoUrl: "https://i.imgur.com/nYoKxRa.mp4",
+    kmsVideoUrl: "",
+  },
+  {
+    id: 22111012,
+    name: "Dragon Dive",
+    mseaVideoUrl: "https://i.imgur.com/RoRRAC4.mp4",
+    kmsVideoUrl: "",
+  },
+  {
+    id: 22140015,
+    name: "Dive of Thunder",
+    mseaVideoUrl: "https://i.imgur.com/waHq89y.mp4",
+    kmsVideoUrl: "",
+  },
+  {
+    id: 22171063,
+    name: "Dragon Breath",
+    mseaVideoUrl: "https://i.imgur.com/AsstFNc.mp4",
+    kmsVideoUrl: "",
+  },
+];
+
+export default skillData;
