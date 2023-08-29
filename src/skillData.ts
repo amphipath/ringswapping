3729244a256afeb119a7d619420550c77591fcc1
amphipath <amphipath@users.noreply.github.com>
2023-08-29 19:19:01 +0800

    add more skills and sort the skills

--- src/skillData.ts
+++ src/skillData.ts
@@ -36,6 +36,50 @@ const skillData: SkillData[] = [
     mseaVideoUrl: "https://i.imgur.com/AsstFNc.mp4",
     kmsVideoUrl: "",
   },
+  {
+    id: 22141017,
+    name: "Magical Residue / Reinforced Magical Residue",
+    mseaVideoUrl: "https://i.imgur.com/O2x0MK1.mp4",
+    kmsVideoUrl: "",
+  },
+  {
+    id: 22170064,
+    name: "Breath - Come Back!",
+    mseaVideoUrl: "https://i.imgur.com/wdRXkpY.mp4",
+    kmsVideoUrl: "",
+  },
+  {
+    id: 400021046,
+    name: "Dragon Break",
+    mseaVideoUrl: "https://i.imgur.com/nnY8h1q.mp4",
+    kmsVideoUrl: "",
+  },
+  {
+    id: 400020051,
+    name: "Break - Come Back!",
+    mseaVideoUrl: "https://i.imgur.com/NdBRMp5.mp4",
+    kmsVideoUrl: "",
+  },
+  {
+    id: 400020046,
+    name: "Imperial Breath",
+    mseaVideoUrl: "https://i.imgur.com/9iDwUf7.mp4",
+    kmsVideoUrl: "",
+  },
+  {
+    id: 22170067,
+    name: "Dive of Earth",
+    mseaVideoUrl: "https://i.imgur.com/mm6508u.mp4",
+    kmsVideoUrl: "",
+  },
+  {
+    id: 22140014,
+    name: "Swift of Thunder",
+    mseaVideoUrl: "https://i.imgur.com/CsSEKgY.mp4",
+    kmsVideoUrl: "",
+  },
 ];
+
+skillData.sort((a: SkillData, b: SkillData) => a.id - b.id);
 
 export default skillData;
